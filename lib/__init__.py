14d40e97b626052ff01cb3605ebb4b902adc272e
EndlessArch <paul.hong0619@gmail.com>
2022-06-27 17:24:23 +0900

    poetry setup & little fix on lexer, parenthesis part

---
+++ lib/__init__.py
@@ -0,0 +1,5 @@
+from . import ast_
+from . import lexer_
+from . import parser_
+from . import trans_
+
---
+++ src/semilang.py
@@ -0,0 +1,27 @@
+# the source of pretty error print
+import pretty_errors
+
+from lib.lexer_ import lexer
+from lib.parser_ import parser
+
+from lib.trans_ import code
+
+import pathlib
+
+def start():
+
+  file_dir = "/cases/if_1.semi"
+
+  input_file = open(''.join([str(pathlib.Path().resolve()), file_dir]), 'r')
+  tokens = lexer.lex('\n'.join(input_file.readlines()))
+  input_file.close()
+
+  ast = parser.parse(tokens)
+
+  print(ast.getast())
+
+  print(code(ast))
+
+  file = open(''.join([str(pathlib.Path().resolve()), '/test.c']), 'w')
+  file.write(code(ast))
+  file.close()
